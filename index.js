96201b20d63a35637645015e2152849b25153f96
Arehman52 <abdurrehman418@gmail.com>
2022-07-30 13:47:51 +0500

    1 file modified

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 
-appDiv.innerHTML = `<h1>JS Starter</h1><br>`;
+appDiv.innerHTML = `
+<h1>JS Mapper Functions</h1>
+<br>
+<p>Open the console to see the results. This mapper function transforms the keys of one object or array of object according to the keys provided in the mappingConfigs object. for each key/value in mappingConfigs, this function will look for the same key value pairs in the response parameter.</p>
+<p> in this example, first parameter i.e. { abc: 'Usman', xyz: 'Mustafa' },
+{ abc: 'Ali', xyz: 'Mustafa' },
+] is array of 2 objects and second parameter i.e. { abc: 'firstName', xyz: 'lastName' }  is the mappingConfigs. so from mappingConfigs it will replace firstName and lastName against each of the abc and xyz entries in the response array of objects.</p>
+`;
 console.log("NOW: ",mapper(
   [
     { abc: 'Usman', xyz: 'Mustafa' },
